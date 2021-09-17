ed8cb26ad01ed9c64abaa6242e716aa6410393d5
geekbrains <chester_095@mail.ru>
2021-09-17 16:33:49 +0300

    Lesson3 Task1: made

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -8,24 +8,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.Locale;
-
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "@@@";
     private TextView decimalNumberEditText;
-    private String number = "";
+    private StringBuilder numberText = new StringBuilder();
     private Button oneDigitButton, twoDigitButton, threeDigitButton, fourDigitButton, fiveDigitButton, sixDigitButton,
-            sevenDigitButton, eightDigitButton, nineDigitButton, pointButton;
-    private boolean checkPoint=true;
+            sevenDigitButton, eightDigitButton, nineDigitButton, zeroDigitButton, pointButton, clearButton, backspaceButton,
+            percentButton, divisionButton, multiplicationButton, subtractionButton, summationButton, equallyButton;
+    private boolean checkPoint = false;
+    private boolean checkMathSymbol = false;
+    private char mathSymbol;
+    private int mathSymbolIndex = 0;
 
 
-    private View.OnClickListener numberClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            onClickNumber(view);
-//            Log.d(TAG, "onClick() - " + numberClickListener);
-        }
-    };
+    private View.OnClickListener numberClickListener = view -> onClickNumber(view);
+    private View.OnClickListener funcButtonClickListener = view -> onClickFunc(view);
+    private View.OnClickListener mathButtonClickListener = view -> onClickMath(view);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +41,16 @@ public class MainActivity extends AppCompatActivity {
         sevenDigitButton = findViewById(R.id.digit_seven_button);
         eightDigitButton = findViewById(R.id.digit_eight_button);
         nineDigitButton = findViewById(R.id.digit_nine_button);
+        zeroDigitButton = findViewById(R.id.digit_zero_button);
         pointButton = findViewById(R.id.point_button);
+        clearButton = findViewById(R.id.clear_button);
+        backspaceButton = findViewById(R.id.backspace_button);
+        percentButton = findViewById(R.id.percent_button);
+        divisionButton = findViewById(R.id.division_button);                // деление
+        multiplicationButton = findViewById(R.id.multiplication_button);    // умножение
+        subtractionButton = findViewById(R.id.subtraction_button);          // вычитание
+        summationButton = findViewById(R.id.summation_button);              // сумма
+        equallyButton = findViewById(R.id.equally_button);                  // равно
 
         oneDigitButton.setOnClickListener(numberClickListener);
         twoDigitButton.setOnClickListener(numberClickListener);
@@ -54,25 +61,108 @@ public class MainActivity extends AppCompatActivity {
         sevenDigitButton.setOnClickListener(numberClickListener);
         eightDigitButton.setOnClickListener(numberClickListener);
         nineDigitButton.setOnClickListener(numberClickListener);
+        zeroDigitButton.setOnClickListener(numberClickListener);
         pointButton.setOnClickListener(numberClickListener);
-
-
+        clearButton.setOnClickListener(funcButtonClickListener);
+        backspaceButton.setOnClickListener(funcButtonClickListener);
+        percentButton.setOnClickListener(funcButtonClickListener);
+        divisionButton.setOnClickListener(mathButtonClickListener);
+        multiplicationButton.setOnClickListener(mathButtonClickListener);
+        subtractionButton.setOnClickListener(mathButtonClickListener);
+        summationButton.setOnClickListener(mathButtonClickListener);
+        equallyButton.setOnClickListener(mathButtonClickListener);
     }
 
-    public void onClickNumber(View view){
+    public void onClickNumber(View view) {
 //        Log.d(TAG, "onClick() - " + view);
+        if (view == oneDigitButton) numberText.append(1);
+        if (view == twoDigitButton) numberText.append(2);
+        if (view == threeDigitButton) numberText.append(3);
+        if (view == fourDigitButton) numberText.append(4);
+        if (view == fiveDigitButton) numberText.append(5);
+        if (view == sixDigitButton) numberText.append(6);
+        if (view == sevenDigitButton) numberText.append(7);
+        if (view == eightDigitButton) numberText.append(8);
+        if (view == nineDigitButton) numberText.append(9);
+        if (view == zeroDigitButton) numberText.append(0);
+        if (view == pointButton && !checkPoint) {
+            checkPoint = true;
+            numberText.append(".");
+        }
+        decimalNumberEditText.setText(numberText);
+    }
 
-        if (view == oneDigitButton) number+="1";
-        if (view == twoDigitButton) number+="2";
-        if (view == threeDigitButton) number+="3";
-        if (view == fourDigitButton) number+="4";
-        if (view == fiveDigitButton) number+="5";
-        if (view == sixDigitButton) number+="6";
-        if (view == sevenDigitButton) number+="7";
-        if (view == eightDigitButton) number+="8";
-        if (view == nineDigitButton) number+="9";
-        if (view == pointButton && checkPoint) {checkPoint=false; number+=".";}
-        decimalNumberEditText.setText(number);
+    public void onClickFunc(View view) {
+        if (view == clearButton) {
+            numberText.delete(0, numberText.length());
+            clearTempVariable();
+        }
+        if (view == backspaceButton && numberText.length() > 0) numberText.deleteCharAt(numberText.length() - 1);
+        decimalNumberEditText.setText(numberText);
+    }
+
+    public void onClickMath(View view) {
+        if (view == summationButton) {
+            if (!checkMathSymbol) {}
+            if ( mathSymbolIndex != numberText.length()) {
+
+            } else {
+                checkMathSymbol = true;
+                mathSymbol = '+';
+                mathSymbolIndex = numberText.length();
+                numberText.append(mathSymbol);
+            }
+        }
+        if (view == divisionButton) {
+            checkMathSymbol = true;
+            mathSymbol = '/';
+            mathSymbolIndex = numberText.length();
+            numberText.append(mathSymbol);
+        }
+        if (view == multiplicationButton) {
+            checkMathSymbol = true;
+            mathSymbol = '*';
+            mathSymbolIndex = numberText.length();
+            numberText.append(mathSymbol);
+        }
+        if (view == subtractionButton) {
+            checkMathSymbol = true;
+            mathSymbol = '-';
+            mathSymbolIndex = numberText.length();
+            numberText.append(mathSymbol);
+        }
+
+        if (view == equallyButton) numberText = mathCalculation(numberText, mathSymbol, mathSymbolIndex);
+        /*  if (view == percentButton) numberText += "3";
+
+         */
+        decimalNumberEditText.setText(numberText);
+    }
+
+    private StringBuilder mathCalculation(StringBuilder numberText, char mathSymbol, int mathSymbolIndex) {
+        StringBuilder result = new StringBuilder();
+        Double firstNumber = Double.valueOf(numberText.substring(0, mathSymbolIndex));
+        Double secondNumber = Double.valueOf(numberText.substring(mathSymbolIndex + 1, numberText.length()));
+        if (mathSymbol == '+') {
+            result.append(firstNumber + secondNumber);
+        } else if (mathSymbol == '-') {
+            result.append(firstNumber - secondNumber);
+        } else if (mathSymbol == '*') {
+            result.append(firstNumber * secondNumber);
+        } else if (mathSymbol == '/') {
+            result.append(firstNumber / secondNumber);
+        } else if (mathSymbol == '%') {
+
+        }
+//            Log.d(TAG, "result=" + result);
+        clearTempVariable();
+        return result;
+    }
+
+    private void clearTempVariable() {
+        mathSymbol = 0;
+        checkMathSymbol = false;
+        checkPoint = true;
     }
 
 }
